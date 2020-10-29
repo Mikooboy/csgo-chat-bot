facb2217c04c4d03444f39c5e2de27ce47467a6d
BuildTools <unconfigured@null.spigotmc.org>
2020-10-30 01:49:56 +0200

    Added error handling to !info

--- cscb.py
+++ cscb.py
@@ -140,111 +140,112 @@ def main():
 
 
 		# Player info
-		if (result[0] == 2):
-			sleep(0.7) # csgo chat cooldown
+		if (result[0] == 4):
+			sleep(0.7)
 
-			kdratio 	= "N/A" # Kill Death Ratio
-			timeplayed 	= "N/A" #time played
-			rounds 		= "N/A"
+			try:
+				kdratio 	= "N/A" # Kill Death Ratio
+				timeplayed 	= "N/A" #time played
+				rounds 		= "N/A"
 
-			game_info 	= True
-			hour_info 	= True
+				game_info 	= True
+				hour_info 	= True
 
-			splitted 	= result[2].decode("utf-8").split(" ")
-			name 		= splitted[len(splitted)-2]
+				splitted 	= result[2].decode("utf-8").split(" ")
+				name 		= splitted[len(splitted)-2]
 
-			matching 	 = 0
-			matching_map = 0
+				matching 	 = 0
+				matching_map = 0
 
-			run(tn, "status")
-			status = tn.expect([b"#end"])
-			lines 	= status[2].decode("utf-8").splitlines()
-			
-			for i in lines:
-				if name.lower() in i.lower():
-					matching = i
-			
-			for i in lines:
-				if "map" in i.lower():
-					matching_map = i
-			
-			mapName = matching_map.split()
-			mapName = mapName[2]
+				run(tn, "status")
+				status = tn.expect([b"#end"])
+				lines  = status[2].decode("utf-8").splitlines()
+				
+				for i in lines:
+					if name.lower() in i.lower():
+						matching = i
+				
+				for i in lines:
+					if "map" in i.lower():
+						matching_map = i
+				
+				mapName = matching_map.split()
+				mapName = mapName[2]
 
-			if (matching == 0) or (len(name) < 2):
-				run(tn, "say Error: Invalid name")
-				print("Error: Invalid name")
-			else:
-				try:
-					if (api_key == ''):
-						raise
-
-					match_split = matching.split()
-
-					playerId = None
-
-					for i in match_split:
-						if "steam_1" in i.lower():
-							playerId = i
-					
-					playerId64 = steamid_to_64bit(playerId)
-
-					print("Request info for: " + str(name))
-
-					url 	= 'https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=' + api_key + '&steamid='
-					url2 	= 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + api_key + '&steamid='
-					final_url 	= url + str(playerId64)
-					final_url2 	= url2 + str(playerId64)
+				if (matching == 0) or (len(name) < 2):
+					if (len(name) >= 2):
+						run(tn, "say Error: Invalid name")
+						print("Error: Invalid name")
+				else:
 					try:
-						r = requests.get(final_url).json()
-						stats = {
-							'total_kills' 	: r['playerstats']['stats'][0]['value'], #total kills
-							'total_deaths' 	: r['playerstats']['stats'][1]['value'], #total deaths
-							'map_rounds'	: "N/A"
-						}
+						match_split = matching.split()
 
-						for i in (r['playerstats']['stats']):
-							id = i['name']
-							if ("rounds" in id) and (mapName in id):
-								stats['map_rounds'] = i['value']
+						playerId = None
 
-						kd = stats['total_kills']/stats['total_deaths'] #kill death ratio
+						for i in match_split:
+							if "steam_1" in i.lower():
+								playerId = i
+						
+						playerId64 = steamid_to_64bit(playerId)
 
-						rounds 	= str(stats['map_rounds'])
-						kdratio 	= str("{:.2f}".format(kd)) # Kill Death Ratio
+						print("Request info for: " + str(name))
+
+						url 	= 'https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=A301D46D7F32FF49C1A24426B8A7D72D&steamid='
+						url2 	= 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=A301D46D7F32FF49C1A24426B8A7D72D&steamid='
+						final_url 	= url + str(playerId64)
+						final_url2 	= url2 + str(playerId64)
+						try:
+							r = requests.get(final_url).json()
+							stats = {
+								'total_kills' 	: r['playerstats']['stats'][0]['value'], #total kills
+								'total_deaths' 	: r['playerstats']['stats'][1]['value'], #total deaths
+								'map_rounds'	: "N/A"
+							}
+
+							for i in (r['playerstats']['stats']):
+								id = i['name']
+								if ("rounds" in id) and (mapName in id):
+									stats['map_rounds'] = i['value']
+
+							kd = stats['total_kills']/stats['total_deaths'] #kill death ratio
+
+							rounds 	= str(stats['map_rounds'])
+							kdratio 	= str("{:.2f}".format(kd)) # Kill Death Ratio
+						except:
+							game_info = False
+
+						try:
+							r2 = requests.get(final_url2).json()
+							games = r2['response']['games'] #total time played
+
+							for i in games:
+								id = i['appid']
+								if (id == 730):
+									time_played = i['playtime_forever']
+
+							hours = time_played/60 #minute to hours
+							timeplayed 	= str(floor(hours)) #time played
+						except:
+							hour_info = False
+						
+						if (not game_info) and (not hour_info):
+							run(tn, "say Error: Game and hour info private")
+							sleep(0.7)
+						elif (not game_info):
+							run(tn, "say Error: Game info private")
+							sleep(0.7)
+						elif (not hour_info):
+							run(tn, "say Error: Hour info private")
+							sleep(0.7)
+
+						run(tn, "say " + str(name) + " | K/D: " + str(kdratio) + " Hours: " + str(timeplayed) + " " + str(mapName) + ": " + str(rounds) + " rounds")
+						print(str(name) + " | K/D: " + str(kdratio) + " Hours: " + str(timeplayed) + " " + str(mapName) + ": " + str(rounds) + " rounds")
 					except:
-						game_info = False
-
-					try:
-						r2 = requests.get(final_url2).json()
-						games = r2['response']['games'] #total time played
-
-						for i in games:
-							id = i['appid']
-							if (id == 730):
-								time_played = i['playtime_forever']
-
-						hours = time_played/60 #minute to hours
-						timeplayed 	= str(floor(hours)) #time played
-					except:
-						hour_info = False
-					
-					if (not game_info) and (not hour_info):
-						run(tn, "say Error: Game and hour info private")
-						sleep(0.7)
-					elif (not game_info):
-						run(tn, "say Error: Game info private")
-						sleep(0.7)
-					elif (not hour_info):
-						run(tn, "say Error: Hour info private")
-						sleep(0.7)
-
-					run(tn, "say " + str(name) + " | K/D: " + str(kdratio) + " Hours: " + str(timeplayed) + " " + str(mapName) + ": " + str(rounds) + " rounds")
-					print(str(name) + " | K/D: " + str(kdratio) + " Hours: " + str(timeplayed) + " " + str(mapName) + ": " + str(rounds) + " rounds")
-				except:
-					run(tn, "say Error: Couldn't retrieve info")
-					print("Error: Couldn't retrieve info")
-					print("Make sure that the API key is correct")
+						run(tn, "say Error: Couldn't retrieve info")
+						print("Error: Couldn't retrieve info")
+			except:
+				run(tn, "say Error: Unknown error")
+				print("say Error: Unknown error")
 
 
 
